aa2b3c93a81954cdde7e17f53cdeee0cec672751
TatBer <orlowka.2013@gmail.com>
2015-10-09 19:34:58 +0200

    Merge branch 'master' of D:\java\Project\webapp06 with conflicts.

--- src/main/ru/javawebinar/webapp/model/Resume.java
+++ src/main/ru/javawebinar/webapp/model/Resume.java
@@ -4,22 +4,34 @@ import java.util.Map;
 import java.util.UUID;
 
 /**
- * GKislin
- * 18.09.2015.
+ * TBerenzon
+ * 01.10.2015
  */
+
 public class Resume {
     private final String uuid;
+    private final String fullName;
     private final Map<ContactType, String> contacts;
     private final Map<SectionType, Section> sections;
 
-    public Resume(Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
-        this(UUID.randomUUID().toString(), contacts, sections);
+    public Resume(String fullName, Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
+        this(UUID.randomUUID().toString(), fullName, contacts, sections);
     }
 
-    public Resume(String uuid, Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
+    public Resume(String uuid, String fullName, Map<ContactType, String> contacts, Map<SectionType, Section> sections) {
         this.uuid = uuid;
+        this.fullName = fullName;
         this.contacts = contacts;
         this.sections = sections;
+    }
+
+    public String getUuid()
+    {
+        return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 
     public Map<ContactType, String> getContacts() {
--- src/main/ru/javawebinar/webapp/store/ArrayStorage.java
+++ src/main/ru/javawebinar/webapp/store/ArrayStorage.java
@@ -1,15 +1,17 @@
 package main.ru.javawebinar.webapp.store;
 
+
+import main.ru.javawebinar.webapp.model.ContactType;
 import main.ru.javawebinar.webapp.model.Resume;
 
 import java.util.Collection;
 
 /**
- * GKislin
- * 02.10.2015.
+ * Created by Tanja on 08.10.2015.
  */
+
 public class ArrayStorage implements IStore {
-    private static final int MAX_LENGTH = 10000;
+    public static final int MAX_LENGTH = 10000;
 
     private Resume[] array = new Resume[MAX_LENGTH];
 
@@ -21,15 +23,29 @@ public class ArrayStorage implements IStore {
     @Override
     public void save(Resume r) {
 
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < MAX_LENGTH; i++) {
+
+            r.add(new Resume(i.toString(), "Berenzon Tanja", ContactType.PHONE("222"), ContactType.ICQ("xxx"),ContactType.MAIL("xcv"),
+        }
+        System.out.println(sb.toString());
+        System.out.println(ArrayStorage.MAX_LENGTH);
+        r.add(new Resume("1000", "Berenzon Tanja", "", "");
+
+
     }
 
     @Override
     public void update(Resume r) {
 
+
     }
 
     @Override
     public Resume load(String uuid) {
+
+
         return null;
     }
 
